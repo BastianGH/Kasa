2678296f02fe8b119bbad8720b3167584bff450f
BastianGH <bastianpussey16@gmail.com>
2023-04-13 20:32:42 +0200

    Initialisation Projet

---
+++ src/layout/Header/index.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+      <header>
+        <img src="" alt=""/>
+        <nav>
+            <ul>
+                <li>
+                    <link></link>
+                </li>
+            </ul>
+        </nav>
+      </header>
+    );
+}
+
+export default Header;
