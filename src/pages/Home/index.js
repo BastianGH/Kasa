e7af43f0504863adcda6a372b74fa6ed5e29130d
BastianGH <bastianpussey16@gmail.com>
2023-04-25 13:55:30 +0200

    initialisation du Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,16 @@
+import Banner from "../../components/Banner";
+import Card from "../../components/Card";
+import locations from "../../logements.json"
+
 function Home() {
   return (
-    <div className="Home">
-        Voici le home
+    <div>
+        <Banner />
+        <div class="locations-container" >
+            {locations.map((location) => (
+              <Card />
+            ))}
+        </div>        
     </div>
   );
 }
