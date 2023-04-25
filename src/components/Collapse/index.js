f400694bc3578125ae4f028c65f2e780b4e19845
BastianGH <bastianpussey16@gmail.com>
2023-04-25 17:47:21 +0200

    Page About terminé

--- src/components/Collapse/index.js
+++ src/components/Collapse/index.js
@@ -1,15 +1,23 @@
 import '../../styles/components/collapse.css';
+import arrow from "../../assets/imgs/arrow.png";
 import React, { useState } from 'react';
 
 function Collapse(props) {
-    const title = props.Title
-    const paragraph = props.Paragraph
-    const open = useState(false)
+    const [open, setOpen] = useState(false);
+    const title = props.Title;
+    const paragraph = props.Paragraph;
+
+    const handleClick = () => {
+      setOpen(!open);
+    };
 
     return (
       <div className="collapse">
-        <button onClick={() => open ? open = false : open = true  }>{title}</button>
-        { open ? <div><p>{paragraph}</p></div> : <div></div> }        
+        <button className="collapse-button" onClick={handleClick}>
+          <span className="collapse-button_text">{title}</span>
+          <img className={`collapse-button_arrow ${open ? 'open' : ''}`} src={arrow} alt="flèche d'ouverture/fermeture" />
+        </button>
+        {open && <div className="collapse-content"><p className="collapse-content_text">{paragraph}</p></div>}
       </div>
     );
 }
