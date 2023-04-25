257aa909d5b3d66319e654cf7c1e0fd8dfccbe9b
BastianGH <bastianpussey16@gmail.com>
2023-04-25 13:31:00 +0200

    initialisation des composants principaux

---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import '../../styles/components/card.css';
+
+function Banner() {
+    return (
+      <div>
+        <img src="" alt="" />
+        <span>Ici la banner</span>
+      </div>
+    );
+}
+
+export default Banner;
---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import '../../styles/components/card.css';
+
+function Card() {
+    return (
+      <figure>
+        <a href="" >
+            <img src="" alt="" />
+            <figcaption>
+                <span>Titre de la location</span>
+            </figcaption>
+        </a>
+      </figure>
+    );
+}
+
+export default Card;
