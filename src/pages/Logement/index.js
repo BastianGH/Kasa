ac40a9d9c7b63cafc8d0989f4519b1e3f89fda3f
BastianGH <bastianpussey16@gmail.com>
2023-04-27 12:10:23 +0200

    commencement page logement

--- src/pages/Logement/index.js
+++ src/pages/Logement/index.js
@@ -1,12 +1,36 @@
 import { useParams } from "react-router-dom";
+import locations from "../../logements.json"
+
+import Carousel from "../../components/Carrousel";
+import Collapse from "../../components/Collapse";
 
 function Logement() {
   const { id } = useParams()
+  const logement = locations.find(location => location.id === id)
 
   return (
-    <div className="Logement">
-        Ceci est le logement {id}
-    </div>
+    <section className="Logement">
+      <Carousel images={logement.pictures} />
+      <div>
+        <div>
+          <h1>{logement.title}</h1>
+          <span>{logement.location}</span>
+        </div>
+        <div>
+          <span>{logement.host.name}</span>
+          <img src={logement.host.picture} alt={logement.host.name} />
+        </div>
+      </div>
+      <div>
+        <div>
+          tags
+        </div>
+        <div>
+          note
+        </div>
+      </div>
+      <Collapse />
+    </section>
   );
 }
 
