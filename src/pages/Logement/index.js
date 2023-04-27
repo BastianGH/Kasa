6a645f4a9cf545402f82bf0bfe515137bf3733e2
BastianGH <bastianpussey16@gmail.com>
2023-04-27 14:55:32 +0200

    avancement de la page Logement

--- src/pages/Logement/index.js
+++ src/pages/Logement/index.js
@@ -1,3 +1,4 @@
+import '../../styles/pages/logement.css';
 import { useParams } from "react-router-dom";
 import locations from "../../logements.json"
 
@@ -11,25 +12,28 @@ function Logement() {
   return (
     <section className="Logement">
       <Carousel images={logement.pictures} />
-      <div>
-        <div>
+      <div className="Logement-first-section">
+        <div className="Logement-first-section_infos">
           <h1>{logement.title}</h1>
           <span>{logement.location}</span>
         </div>
-        <div>
+        <div className="Logement-first-section_host">
           <span>{logement.host.name}</span>
           <img src={logement.host.picture} alt={logement.host.name} />
         </div>
       </div>
-      <div>
-        <div>
-          tags
+      <div className="Logement-second-section">
+        <div className="Logement-second-section_tags">
+          {logement.tags}
         </div>
-        <div>
-          note
+        <div className="Logement-second-section_rates">
+          {logement.rating}
         </div>
       </div>
-      <Collapse />
+      <div className="Logement-third-section">
+        <Collapse className="Logement-third-section_details" Title="Description" />
+        <Collapse className="Logement-third-section_items" Title="Équipements" />
+      </div>
     </section>
   );
 }
