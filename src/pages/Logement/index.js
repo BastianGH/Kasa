e9321272dd7a9d396a4fe4534b3fcd38e1c6e643
BastianGH <bastianpussey16@gmail.com>
2023-05-04 13:29:59 +0200

    modifs page logement

--- src/pages/Logement/index.js
+++ src/pages/Logement/index.js
@@ -12,27 +12,27 @@ function Logement() {
   return (
     <section className="Logement">
       <Carousel images={logement.pictures} />
-      <div className="Logement-first-section">
-        <div className="Logement-first-section_infos">
-          <h1>{logement.title}</h1>
-          <span>{logement.location}</span>
+      <div className="Logement-introduction">
+        <div className="Logement-introduction_infos">
+          <h1 className="Logement-introduction_infos-title" >{logement.title}</h1>
+          <span className="Logement-introduction_infos-location" >{logement.location}</span>
+          <div className="Logement-introduction_infos-tags" >
+            {logement.tags}
+          </div>
         </div>
-        <div className="Logement-first-section_host">
-          <span>{logement.host.name}</span>
-          <img src={logement.host.picture} alt={logement.host.name} />
+        <div className="Logement-introduction_host&rate">
+          <div className="Logement-introduction_host">
+            <span className="Logement-introduction_host-name">{logement.host.name}</span>
+            <img className="Logement-introduction_host-photo" src={logement.host.picture} alt={logement.host.name} />
+          </div>
+          <div className="Logement-introduction_rating">
+            {logement.rating}
+          </div>
         </div>
       </div>
-      <div className="Logement-second-section">
-        <div className="Logement-second-section_tags">
-          {logement.tags}
-        </div>
-        <div className="Logement-second-section_rates">
-          {logement.rating}
-        </div>
-      </div>
-      <div className="Logement-third-section">
-        <Collapse className="Logement-third-section_details" Title="Description" />
-        <Collapse className="Logement-third-section_items" Title="Équipements" />
+      <div className="Logement-details">
+        <Collapse className="Logement-details_details" Title="Description" />
+        <Collapse className="Logement-details_items" Title="Équipements" />
       </div>
     </section>
   );
